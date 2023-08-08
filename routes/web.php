e27c3858adf1edf2f7d64934b8d0a6983c7c74fe
Carmelo Jules Bingcalan Marilag <carmelomarilag39@gmail.com>
2023-08-08 18:05:18 +0800

    eloquent

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,9 @@
 <?php
 
+use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
-
+use Illuminate\Support\Facades\DB;
+use App\Models\User;
 /*
 |--------------------------------------------------------------------------
 | Web Routes
@@ -13,4 +15,64 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::view('/', 'welcome');
+Route::get('/', function () {
+    // return view('welcome');
+    //select or return dbfacade, query builder and eloquent
+    //$users = DB:: select ("Select * from users");
+    // $users = DB::table('users')->get();
+    //$users = DB::table('users')->first();
+    //$users = User::where('id', 1)->first();
+
+
+    //insert dbfacade, query builder and eloquen
+    //$user = DB:: insert ('insert into users (name, email, password) values (?,?,?)', [
+    // 'Jules',
+    // 'carmelomarilag@yahoo.com',
+    // 'password',
+    //]);
+    //$users = DB::table('users')->insert([
+    //    'name' => 'jules',
+    //    'email' => 'jules@yahoo.com',
+    //    'password' => 'password',
+    //]);
+    // $users =User::create([
+    //     'name' => 'jules',
+    //     'email' => 'jules@yahoo.com',
+    //     'password' => 'password',
+    // ]);
+
+    //update dbfacade, query builder and eloquen
+    //$user = DB:: update ("update users set email = ? where id = ?", [
+    //'zxcvbn@gmail.com',
+    // 2,
+    // ]);
+    //$users = DB::table('users')->where('id', 3)->update([
+    //       'name' => 'melojules'
+    //    ]);
+    // $users = User::find(4);
+    // $users ->update([
+    //     'name' => 'melojules1'
+    // ]);
+
+    //delete dbfacade, query builder and eloquen
+    //$user = DB:: select ("delete from users where id =?", [
+    //2
+    //]);
+    //$users = DB::table('users')->where('id', 3)->delete();
+    // $users = User::find(4);
+    // $users->delete();
+    
+   dd($users);
+});
+
+Route::get('/dashboard', function () {
+    return view('dashboard');
+})->middleware(['auth', 'verified'])->name('dashboard');
+
+Route::middleware('auth')->group(function () {
+    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
+    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
+    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+});
+
+require __DIR__.'/auth.php';
