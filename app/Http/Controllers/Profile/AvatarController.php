b29a2912493c266cdcda7d87264aba38b78040a4
Carmelo Jules Bingcalan Marilag <carmelomarilag39@gmail.com>
2023-08-10 09:36:12 +0800

    youtube video 19

---
+++ app/Http/Controllers/Profile/AvatarController.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace App\Http\Controllers\Profile;
+
+use App\Http\Controllers\Controller;
+use App\Http\Requests\UpdateAvatarRequest;
+
+class AvatarController extends Controller
+{
+    public function update(UpdateAvatarRequest $request)
+    {
+
+        dd($request->all());
+
+        return redirect(route('profile.edit'));
+    }
+}
--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Http\Controllers\ProfileController;
+use App\Http\Controllers\Profile\AvatarController;
 use Illuminate\Support\Facades\Route;
 use Illuminate\Support\Facades\DB;
 use App\Models\User;
@@ -75,6 +76,7 @@ Route::middleware('auth')->group(function () {
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+    Route::patch('/profile/avatar', [AvatarController::class, 'update'])->name('profile.avatar');
 });
 
 require __DIR__.'/auth.php';
